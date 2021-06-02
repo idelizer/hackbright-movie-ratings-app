4d4cebb860b2dbade73b3dd3e8aebbbf3a2eada7
Eliza Cantwell <elizaofthebay@gmail.com>
2021-06-02 04:04:37 +0000

    Almost finished part 3; up to displaying users

--- crud.py
+++ crud.py
@@ -24,6 +24,16 @@ def create_movie(title, overview, release_date, poster_path):
 
     return movie
 
+def get_movies():
+    """Return all movies."""
+
+    return Movie.query.all()
+
+def get_movie_by_id(movie_id):
+    """Return a movie by its id."""
+
+    return Movie.query.get(movie_id)
+
 def create_rating(user, movie, score):
     """Create and return a new rating."""
 
